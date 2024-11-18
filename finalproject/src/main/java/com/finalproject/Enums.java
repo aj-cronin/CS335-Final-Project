57042faf646f7d4fa32e87776a3c467e1a8b3e0a
Aidan Tucker <97992481+artboy110702@users.noreply.github.com>
2024-11-18 16:07:02 -0700

    Added: enum DIRECTION
    for switching directions in Board class

--- finalproject/src/main/java/com/finalproject/Enums.java
+++ finalproject/src/main/java/com/finalproject/Enums.java
@@ -3,4 +3,8 @@ public class Enums{
         // didn't know what to name them, but 11 values for each Tile
         EX1, EX2, EX3, EX4, EX5, EX6, EX7, EX8, EX9, EX10, EX11;
     }
+
+    public enum DIRECTION{
+        // for Board class
+        LEFT, RIGHT, UP, DOWN;
 }
