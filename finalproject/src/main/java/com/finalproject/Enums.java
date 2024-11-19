713f74de1c336212187c7699a950289ac98398fe
Nathan <148111051+nosborne159@users.noreply.github.com>
2024-11-18 19:24:13 -0700

    Enums-w/-DIRECTION to staging (#3)
    * Added Tile and Enums class
    
    * Added: enum DIRECTION
    
    for switching directions in Board class
    
    ---------
    
    Co-authored-by: bstetson04 <bstetson@arizona.edu>
    Co-authored-by: Aidan Tucker <97992481+artboy110702@users.noreply.github.com>

--- finalproject/src/main/java/com/finalproject/Enums.java
+++ finalproject/src/main/java/com/finalproject/Enums.java
@@ -1,7 +1,13 @@
-package finalproject;
+public class Enums{
+  
+    public enum COLOR{
+        // didn't know what to name them, but 11 values for each Tile
+        EX1, EX2, EX3, EX4, EX5, EX6, EX7, EX8, EX9, EX10, EX11;
+    }
 
-public class Enums {
-	public enum DIRECTION{
-		LEFT, RIGHT, UP, DOWN;
-	}
+    public enum DIRECTION{
+        // for Board class
+        LEFT, RIGHT, UP, DOWN;
+    }
+
 }
