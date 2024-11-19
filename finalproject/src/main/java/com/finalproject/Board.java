364c5660b3da9607c0ab833f4118304a794adede
Nathan <148111051+nosborne159@users.noreply.github.com>
2024-11-18 19:19:38 -0700

    add: made the moveLeft, moveRight, and toString methods

--- finalproject/src/main/java/com/finalproject/Board.java
+++ finalproject/src/main/java/com/finalproject/Board.java
@@ -4,39 +4,130 @@ import java.util.ArrayList;
 
 public class Board {
 	
-	private ArrayList<ArrayList<Integer>> board;
+	private ArrayList<ArrayList<Tile>> board;
+	private boardX;
+	private boardY;
 	
 	public Board() {
-		board = new ArrayList<ArrayList<Integer>>();
+		this.boardX = 4;
+		this.boardY = 4;
+		// inicialize board to a certain size
+		this.board = new ArrayList<ArrayList<Tile>>(this.boardY);
+		for (int ii = 0; ii < this.boardY; ii++){
+			this.board.add(new ArrayList<Tile>(this.boardX));
+		}
 	}
 	
-	public ArrayList<ArrayList<Integer>> getBoard() {
-		return board;
+	public ArrayList<ArrayList<Tile>> getBoard() {
+		// make a copy
+		ArrayList<Arraylist<Tile>> temp = new ArrayList<Arraylist<Tile>>(this.boardY);
+		for (int ii = 0; ii < boardY; ii++){
+			temp.get(ii) = new ArrayList<Tile>(this.board.get(ii));
+		}
+		return this.board;
 	}
 	
 	public void move(Direction direction) {
 		switch (direction) {
 		case LEFT:
-			moveLeft();
+			this.moveLeft();
 			break;
 		case RIGHT:
-			moveLeft();
+			this.moveLeft();
 			break;
 		case UP:
-			moveUp();
+			this.moveUp();
 			break;
 		case DOWN:
-			moveDown();
+			this.moveDown();
 			break;
 		}
 	}
 	
 	private void moveLeft() {
-		// TODO
+		for (int ii = 0; ii < this.boardY; ii++){
+			// whether or no the tile before this was merged(to prevent excess merging)
+			boolean combined = false;
+			for (int jj = 1; jj < this.boardX; jj++){
+				// empty space
+				if (this.board.get(ii).get(jj) == null){
+					continue;
+				}
+				// space with a tile
+				// find the tile to the left that has something
+				int currentIndex = jj-1;
+				Tile left = this.board.get(ii).get(currentIndex)
+				while (left == null && currentIndex > 0){	
+					currentIndex--;
+					if (currentIndex <= 0){
+						left = this.board.get(ii).get(currentIndex);
+					}
+				}
+
+				if (left != null){
+					// check if they can combine and left was not combined this round
+					if (left.CHECKIFTHETILESCANCOMBINE(this.board.get(ii).get(jj)) && !combined){
+						// combine the two tiles
+						left.SOMEHOWCOMBINETHETILES(this.board.get(ii).get(jj));
+						combined = true;
+					// dont merge tiles, just put the current next to the next tile to the left
+					} else {
+						this.board.get(ii).set(currentIndex, this.board.get(ii).get(jj));
+						this.board.get(ii).set(jj, null);
+						combined = false;
+					}
+				} else {
+					// dont merge tiles, just put the current next to the next tile to the left or the wall
+					this.board.get(ii).set(currentIndex, this.board.get(ii).get(jj));
+					this.board.get(ii).set(jj, null);
+					combined = false;
+				}
+				
+			}
+		}
 	}
 	
 	private void moveRight() {
-		// TODO
+		for (int ii = 0; ii < this.boardY; ii++){
+			// whether or no the tile before this was merged(to prevent excess merging)
+			boolean combined = false;
+			for (int jj = this.boardX - 1; jj > 0; jj--){
+				// empty space
+				if (this.board.get(ii).get(jj) == null){
+					continue;
+				}
+				// space with a tile
+				// find the tile to the right that has something
+				int currentIndex = jj+1;
+				Tile right = this.board.get(ii).get(currentIndex)
+				while (right == null && currentIndex < boardX - 1){	
+					currentIndex++;
+					if (currentIndex <= boardX - 1){
+						right = this.board.get(ii).get(currentIndex);
+					}
+				}
+
+				if (right != null){
+					// check if they can combine and right was not combined this round
+					if (right.CHECKIFTHETILESCANCOMBINE(this.board.get(ii).get(jj)) && !combined){
+						// combine the two tiles
+						right.SOMEHOWCOMBINETHETILES(this.board.get(ii).get(jj));
+						combined = true;
+					// dont merge tiles, just put the current next to the next tile to the right
+					} else {
+						this.board.get(ii).set(currentIndex, this.board.get(ii).get(jj));
+						this.board.get(ii).set(jj, null);
+						combined = false;
+					}
+				} else {
+					// dont merge tiles, just put the current next to the next tile to the right or the wall
+					this.board.get(ii).set(currentIndex, this.board.get(ii).get(jj));
+					this.board.get(ii).set(jj, null);
+					combined = false;
+				}
+				
+			}
+		}
 	}
 	
 	private void moveUp() {
@@ -45,6 +136,21 @@ public class Board {
 	
 	private void moveDown() {
 		// TODO
+	}
+
+	public String toString(){
+		String out = "┌" + "----".repeat(this.boardX - 1) + "---┐";
+		for (int ii = 0; ii < this.boardY; ii++){
+			out += "\n|";
+			for (int jj = 0; jj < this.boardX; jj++){
+				out += " " + this.board.get(ii).get(jj).getValue() + " |";
+			}
+			if (ii != this.boardY - 1){
+    			out += "\n|" + "---+".repeat(this.boardX - 1) + "---|";
+			}
+		}
+		out += "\n└" + "----".repeat(this.boardX - 1) + "---┘";
+		return out;
 	}
 	
 }
