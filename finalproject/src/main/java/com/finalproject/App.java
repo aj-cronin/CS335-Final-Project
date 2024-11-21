a2d9b35ce556a63ba18828cae2b783d66dcf2fdb
aj-cronin <aj.cronin14@gmail.com>
2024-11-21 13:31:57 -0700

    Added: Starting GUI Fixed: TextGame closing input

--- finalproject/src/main/java/com/finalproject/App.java
+++ finalproject/src/main/java/com/finalproject/App.java
@@ -1,9 +1,12 @@
 package com.finalproject;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,28 +16,48 @@ import java.io.IOException;
  */
 public class App extends Application {
 
-
-    // Woohoo this is my comment. I am the walrus.
     private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
 
     public static void main(String[] args) {
         launch();
     }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setTitle("2048");
+        Group root = new Group();
+
+        scene = new Scene(root, 980, 640);
+        scene.setFill(Color.web("FAF8F0"));
+        stage.setScene(scene);
+
+        showTitle(root);
+        showWIP(root);
+        
+
+        stage.show();
+    }
+
+    private void showTitle(Group root) {
+        Text title = new Text("2048");
+        title.setX(430);
+        title.setY(60);
+        title.setTextAlignment(TextAlignment.CENTER);
+        title.setFill(Color.web("756452"));
+        title.setFont(Font.loadFont("file:FinalProject/src/main/resources/fonts/ClearSans-Bold.ttf", 48));
+        root.getChildren().add(title);
+    }
+
+    private void showWIP(Group root) {
+        Text WIP = new Text("Under Construction...");
+        WIP.setX(275);
+        WIP.setY(320);
+        WIP.setTextAlignment(TextAlignment.CENTER);
+        WIP.setFill(Color.web("756452"));
+        WIP.setFont(Font.loadFont("file:FinalProject/src/main/resources/fonts/ClearSans-Regular.ttf", 48));
+        root.getChildren().add(WIP);
+    }
+
+    
 
 }
