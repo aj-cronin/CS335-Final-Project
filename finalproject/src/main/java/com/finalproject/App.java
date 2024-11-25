286fbba4886e9e3246ebc0c63e39ca4bcb0fc40f
bstetson04 <bstetson@arizona.edu>
2024-11-25 12:05:10 -0700

    Added showBoard() and began updateTiles() in App.java

--- finalproject/src/main/java/com/finalproject/App.java
+++ finalproject/src/main/java/com/finalproject/App.java
@@ -1,8 +1,14 @@
 package com.finalproject;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -10,6 +16,7 @@ import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * JavaFX App
@@ -17,11 +24,16 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private int start = 25;
+    // can change to make board size dynamic
+    private int boardX = start + 4;
+    private int boardY = start + 4;
+    private GridPane board = new GridPane();
 
     public static void main(String[] args) {
         launch();
     }
-
+    
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("2048");
@@ -32,7 +44,8 @@ public class App extends Application {
         stage.setScene(scene);
 
         showTitle(root);
-        showWIP(root);
+        //showWIP(root);
+        showBoard(root);
         
 
         stage.show();
@@ -54,10 +67,47 @@ public class App extends Application {
         WIP.setY(320);
         WIP.setTextAlignment(TextAlignment.CENTER);
         WIP.setFill(Color.web("756452"));
-        WIP.setFont(Font.loadFont("file:FinalProject/src/main/resources/fonts/ClearSans-Regular.ttf", 48));
+        WIP.setFont(Font.loadFont("file:finalproject/src/main/resources/fonts/ClearSans-Regular.ttf", 48));
         root.getChildren().add(WIP);
     }
 
+    // creates an empty board of tiles
+    private void showBoard(Group root) {
+        // creating a grid to emulate the board and centering it
+        board.setAlignment(Pos.CENTER);
+        board.setHgap(5);
+        board.setVgap(5); 
+        board.setPadding(new Insets(20));
+
+        // fill in 
+        double tileSize = 100;
+        
+        for (int row = start; row < boardX; row++) {
+            for (int col = start + 25; col < boardY + 25; col++) {
+                StackPane tile = new StackPane();
+                tile.setMinSize(tileSize, tileSize);
+                tile.setPrefSize(tileSize, tileSize);
+                // can change color later
+                tile.setStyle("-fx-background-color: #000000;");
+
+                // Add the tile to the GridPane
+                board.add(tile, col, row);
+            }
+        }
+
+        root.getChildren().add(board);
+    }
+    
+
+    public void updateTiles(ArrayList<Tile> boardList){
+        
+        for(int row = 0; row < boardList.size(); row ++){
+            Tile tmpTile = boardList.get(row);
+            if(tmpTile != null){
+                
+            }
+        }
+    }
     
 
 }
