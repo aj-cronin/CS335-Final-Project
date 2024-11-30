c663d92803d9b9bce0b30ea873cfb9fa4ffbab5f
aj-cronin <aj.cronin14@gmail.com>
2024-11-30 13:30:57 -0700

    Added: Basic Sound Functionality

--- finalproject/src/main/java/com/finalproject/App.java
+++ finalproject/src/main/java/com/finalproject/App.java
@@ -8,6 +8,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.shape.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -20,8 +21,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import org.w3c.dom.css.Rect;
 
 /**
  * JavaFX App
@@ -50,14 +49,67 @@ public class App extends Application {
         scene.setFill(Color.web("FAF8F0"));
         stage.setScene(scene);
 
-        Leaderboard myLeaderboard = new Leaderboard("FinalProject/src/main/java/com/finalproject/leaderboard.csv");
+        Leaderboard myLeaderboard = new Leaderboard("finalproject/src/main/resources/data/leaderboard.csv");
 
-        showTitle(root);
-        showLeaderboardButton(root, myLeaderboard);
-        showBoard(root);
+        SoundEffects.playBackgroundMusic();
+        showEverything(root, myLeaderboard);
         
-
         stage.show();
+    }
+
+    private void showEverything(Group root, Leaderboard lb) {
+        showTitle(root);
+        showBoard(root);
+        showLeaderboardButton(root, lb);
+        showSoundButton(root);
+        showVolumeControl(root);
+    }
+
+    private void showVolumeControl(Group root) {
+        // Easy Values for Quick Changes to the whole group.
+        int XOFFSET = 50;
+        int YOFFSET = -10;
+        
+        // Adds border behind volume control.
+        Rectangle border = new Rectangle(160, 50);
+        border.setFill(Color.web("756452"));
+        border.setX(750+XOFFSET);
+        border.setY(25+YOFFSET);
+        border.setArcWidth(15);
+        border.setArcHeight(15);
+
+        // Creates text for volume number that updates based on slider.
+        Text volumeNumber = new Text("Volume: 100");
+        volumeNumber.setLayoutX(765+XOFFSET);
+        volumeNumber.setLayoutY(47+YOFFSET);
+        volumeNumber.setFill(Color.WHITE);
+        volumeNumber.setFont(Font.loadFont("file:FinalProject/src/main/resources/fonts/ClearSans-Regular.ttf", 15));
+
+        // Creates slider that can adjust the volume.
+        Slider slider = new Slider(0.0, 1.0, 1.0);
+        slider.setLayoutX(760+XOFFSET);
+        slider.setLayoutY(52+YOFFSET);
+        slider.setOnMouseDragged((e) -> {
+            SoundEffects.setVolume(slider.getValue());
+            volumeNumber.setText("Volume: " + SoundEffects.getVolume().toString());
+        });
+
+        // Add them to root.
+        root.getChildren().add(border);
+        root.getChildren().add(slider);
+        root.getChildren().add(volumeNumber);
+    }
+
+
+    private void showSoundButton(Group root) {
+        Button soundButton = new Button("Play Sound");
+        soundButton.setLayoutX(850);
+        soundButton.setLayoutY(300);
+        soundButton.setOnAction((e) -> {
+           SoundEffects.playMoveSound(); 
+        });
+
+        root.getChildren().add(soundButton);
     }
 
     private void showTitle(Group root) {
@@ -72,7 +124,7 @@ public class App extends Application {
 
     private void showLeaderboardButton(Group root, Leaderboard lb) {
         Button lbButton = new Button("Leaderboard");
-        lbButton.setLayoutX(850);
+        lbButton.setLayoutX(100);
         lbButton.setOnAction((e) -> {
            displayLeaderboard(root, lb); 
         });
@@ -90,20 +142,19 @@ public class App extends Application {
         lbText.setX(400);
         lbText.setY(200);
 
-        Rectangle coverBoard = new Rectangle(500, 500);
+        Rectangle coverBoard = new Rectangle(550, 500);
         coverBoard.setFill(Color.web("FAF8F0"));
+        coverBoard.setOpacity(0.8);
         coverBoard.setX(200);
         coverBoard.setY(100);
 
         Button exitButton = new Button("Close");
 
-        exitButton.setLayoutX(850);
+        exitButton.setLayoutX(100);
         exitButton.setLayoutY(45);
         exitButton.setOnAction((e) -> {
             root.getChildren().clear();
-            showTitle(root);
-            showLeaderboardButton(root, lb);
-            showBoard(root);
+            showEverything(root, lb);
         });
 
         root.getChildren().add(coverBoard);
@@ -114,6 +165,14 @@ public class App extends Application {
 
     // creates an empty board of tiles and hashes each StackPane used as a tile to the gridMap
     private void showBoard(Group root) {
+        // Add border behind tiles
+        Rectangle border = new Rectangle(450, 450);
+        border.setX(252);
+        border.setY(127);
+        border.setFill(Color.web("9B8A7C"));
+        border.setArcHeight(25);
+        border.setArcWidth(25);
+
         // creating a grid to emulate the board and centering it
         board.setAlignment(Pos.CENTER);
         board.setHgap(5);
@@ -137,6 +196,7 @@ public class App extends Application {
             }
         }
 
+        root.getChildren().add(border);
         root.getChildren().add(board);
     }
     
---
+++ finalproject/src/main/java/com/finalproject/SoundEffects.java
@@ -0,0 +1,62 @@
+/**
+ * SoundEffects.java
+ * 
+ * This is a class that has a lot of fields and methods that are static because we know that every time
+ * we want to use sound effects, they will be the same. Every instance of SoundEffects would be the same
+ * so it makes the most sense to just have things be called statically.
+ */
+package com.finalproject;
+
+import javafx.scene.media.*;
+import java.io.File;
+import java.util.ArrayList;
+
+public class SoundEffects {
+    // Music to be constantly playing throughout the game.
+    private static MediaPlayer backgroundMusic = new MediaPlayer(new Media(new File("finalproject/src/main/resources/sounds/backgroundMusic.wav").toURI().toString()));
+
+    // Sound effects that will be played based on actions in the game.
+    private static MediaPlayer moveSound = new MediaPlayer(new Media(new File("finalproject/src/main/resources/sounds/woosh.wav").toURI().toString()));
+    private static MediaPlayer newTileSound = new MediaPlayer(new Media(new File("finalproject/src/main/resources/sounds/pop.wav").toURI().toString()));
+    private static MediaPlayer combineSound = new MediaPlayer(new Media(new File("finalproject/src/main/resources/sounds/combine.wav").toURI().toString()));
+    
+    // Collection of sounds for when updating volume.
+    private static ArrayList<MediaPlayer> sounds = new ArrayList<MediaPlayer>();
+
+    static {
+        sounds.add(backgroundMusic);
+        sounds.add(moveSound);
+        sounds.add(newTileSound);
+        sounds.add(combineSound);
+    }
+
+    public static void playBackgroundMusic() {
+        backgroundMusic.setCycleCount(MediaPlayer.INDEFINITE);
+        backgroundMusic.play();
+    }
+
+    public static void playMoveSound() {
+        moveSound.play();
+        moveSound.setOnEndOfMedia(moveSound::stop);
+    }
+
+    public static void playNewTileSound() {
+        newTileSound.play();
+        newTileSound.setOnEndOfMedia(newTileSound::stop);
+    }
+
+    public static void playCombineSound() {
+        combineSound.play();
+        newTileSound.setOnEndOfMedia(newTileSound::stop);
+    }
+    
+    public static void setVolume(double vol) {
+        for(MediaPlayer sound : sounds) {
+            sound.setVolume(vol);
+        }
+    }
+
+    public static Integer getVolume() {
+        return (int) (moveSound.getVolume() * 100);
+    }
+}
