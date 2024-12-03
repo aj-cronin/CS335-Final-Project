87220922918ee2ce193a6ac05847fceac02406a9
aj-cronin <aj.cronin14@gmail.com>
2024-12-03 13:14:50 -0700

    Fixed: Combine Sound Happening only once

--- finalproject/src/main/java/com/finalproject/SoundEffects.java
+++ finalproject/src/main/java/com/finalproject/SoundEffects.java
@@ -47,7 +47,7 @@ public class SoundEffects {
 
     public static void playCombineSound() {
         combineSound.play();
-        newTileSound.setOnEndOfMedia(newTileSound::stop);
+        newTileSound.setOnEndOfMedia(combineSound::stop);
     }
     
     public static void setVolume(double vol) {
