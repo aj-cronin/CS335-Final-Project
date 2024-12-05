76e2ae7ee93c3c42df248cb59227e163dc822906
bstetson04 <bstetson@arizona.edu>
2024-12-05 10:31:08 -0700

    Added some documentation!

--- finalproject/src/main/java/com/finalproject/Enums.java
+++ finalproject/src/main/java/com/finalproject/Enums.java
@@ -1,11 +1,21 @@
+/**
+ * @authors Aidan Tucker, AJ Cronin, Brooke Stetson, Nathan Osborne
+ * @file Enums.java
+ * @purpose Provides the enum used within our classes to avoid primitive obsession. DIRECTION is
+ * used to represent the choices of movement the user can choose from to move on the game board.
+ */
+
+
 package com.finalproject;
 
+/**
+ * Enums provided to represent game functionality.
+ */
 public class Enums{
 
-
+    // Represents movement within the game board
     public enum DIRECTION{
-        // for Board class
+        // The four options for movement
         LEFT, RIGHT, UP, DOWN;
     }
-
 }
